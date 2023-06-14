592ada7b45e8c45c4aafa93370c6f38b07784c66
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-06-14 14:00:53 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `On Saturdays, I like to ${activity}!`;
+  }
+
+  function mondayWork(task = 'go to the office') {
+    return `On Mondays, I need to ${task}.`;
+  }
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
